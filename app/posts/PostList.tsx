c8225829f4711e62aa597ed9eb28f44df6543b32
Ángel Díaz <diazangel9311@gmail.com>
2023-10-06 21:55:58 -0500

    Improve styles for post list
    Add .next and build to .prettierignore and .eslintignore

--- app/posts/PostList.tsx
+++ app/posts/PostList.tsx
@@ -16,7 +16,11 @@ async function PostList() {
     </li>
   ));
 
-  return <ul className="grid grid-cols-1 sm:grid-cols-3 xl:grid-cols-4 gap-3">{mappedPosts}</ul>;
+  return (
+    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-3">
+      {mappedPosts}
+    </ul>
+  );
 }
 
 export default PostList;
