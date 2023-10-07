750b8937899a392554d0768edccbb6186ef73f07
Ángel Díaz <diazangel9311@gmail.com>
2023-10-06 19:11:16 -0500

    Implement not-found for undefined post

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,22 +1,31 @@
 import content from '@/app/content/posts.json';
 import { Params } from 'next/dist/shared/lib/router/utils/route-matcher';
 import PostContent from './PostContent';
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
 
 async function getPost(id: number | string) {
   if (Number.isInteger(Number(id))) {
     const post = content.posts.find(p => p.id === Number(id));
     return post;
   }
+
   return undefined;
 }
 
 async function PostContentPage({ params }: { params: Params}) {
   const post = await getPost(params.id);
+  if (!post) {
+    notFound();
+  }
 
   return (
-    <>
-      {post ? <PostContent {...post} /> : <div>HOLA</div>}
-    </>
+    <main>
+      <section>
+        <Link href="/">Back</Link>
+        <PostContent {...post} />
+      </section>
+    </main>
   )
 }
 
