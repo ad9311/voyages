cee620fdd07d3ad51102c896f76870977ed7bf83
Ángel Díaz <diazangel9311@gmail.com>
2023-10-09 07:38:30 -0500

    Fix typo in not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ function NotFound() {
     <main>
       <div className="mt-80 mx-auto container text-center">
         <h2 className="text-primary-dark text-3xl font-bold">NOT FOUND</h2>
-        <p className="text-secondary text-xl">The page you were looking for does not exists</p>
+        <p className="text-secondary text-xl">The page you were looking for does not exist</p>
       </div>
     </main>
   );
